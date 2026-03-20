6df12dd2e691f874b2a2045c72ea5526715a7c2b
ellaybeckstrom <ellay.bry2@gmail.com>
2026-03-20 09:35:07 +0100

    lagt til pseudokode for newtons metode

---
+++ oblig2.py
@@ -0,0 +1,33 @@
+import matplotlib as plt
+import numpy as np
+
+#PSEUDO 1
+"""
+def f(x):
+    y = funksjon
+    return y
+
+def df(x):
+    y = funksjon
+    return y
+
+
+plotter funksjonen for å se ca hvor jeg kan begynne
+med newtons metode.
+
+Newtons metode
+def newton_metode(x):
+    y = x - (f(x)/df(x))
+    return y
+
+    
+x0 = vurderes utifra plott
+
+y = 0
+
+for i in range (n):
+    y = newton_metode(x0)
+    x0 = y
+
+print("0 punktet til funksjonen ligger på:", y)
+"""
